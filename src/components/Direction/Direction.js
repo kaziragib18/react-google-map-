8521bac43da02eb4f43aa330ad646e4001771cd4
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-10-21 00:18:38 +0600

    map direction explore

--- src/components/Direction/Direction.js
+++ src/components/Direction/Direction.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
 
-const Direction = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+
+const location = {
+      lat: 22.357733738899825,
+      lng: 91.82012551464157
+};
+
+const Direction = ({ origin, destination }) => {
+    const [response, setResponse] = useState(null);
+    const directionsCallback = res => {
+        if (res != null) {
+            setResponse(res);
+        }
+    }
+    return (
+        <div>
+            <LoadScript
+            //need google api here
+                googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+            >
+                <GoogleMap
+                    id='direction-example'
+                    mapContainerStyle={{
+                        height: '100vh',
+                        width: '100%'
+                    }}
+                    // required
+                    zoom={14}
+                    // required
+                    center={location}
+                >
+
+                    <DirectionsService
+                        // required
+                        options={{
+                            origin: origin,
+                            destination: destination,
+                            travelMode: 'DRIVING'
+                        }}
+                        // required
+                        callback={directionsCallback}
+                    />
+
+
+                    {
+                        response !== null && (
+                            <DirectionsRenderer
+                                // required
+                                options={{
+                                    directions: response
+                                }}
+                            />
+                        )
+                    }
+                </GoogleMap>
+            </LoadScript>
+        </div>
+    );
 };
 
 export default Direction;
